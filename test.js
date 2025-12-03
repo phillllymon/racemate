b4f56bef0b6479db843028a3b22460f288dcd245
Parker Harris <rparkerharris@gmail.com>
2025-12-03 14:34:14 -0800

    Update users endpoint to use built in auth endpoint

--- test.js
+++ test.js
@@ -1,21 +1,14 @@
 async function test() {
-    const url = "https://racematevercel.vercel.app/api/users/index.js"; // <-- replace this
-    // const url = "https://racematevercel.vercel.app/api/hello"; // <-- replace this
-  
-    const response = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            email: "testuser@example.com",
-            password: "mypassword123"
-        })
+    const url = "https://racematevercel.vercel.app/api/users"; // your project URL
+    const resp = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: "testuser@example.com", password: "mypassword123" })
     });
   
-    const result = await response;
-    console.log(result);
-    result.json().then((r) => {
-        console.log(r);
-    })
-}
-
-test().catch(console.error);
+    console.log("Status:", resp.status);
+    const txt = await resp.text();
+    console.log("Body:", txt);
+  }
+  
+  test().catch(console.error);
