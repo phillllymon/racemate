a14fde09b8f87398ce56711968cd15b98c22e2cb
Parker Harris <rparkerharris@gmail.com>
2025-12-03 13:43:15 -0800

    Include testQuery in main app

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 
-// DEV ONLY
-const endpoint = "https://racematevercel.vercel.app";
+// // DEV ONLY
+// const endpoint = "https://racematevercel.vercel.app";
 
-// PROD
-// const endpoint = "";
+// // PROD
+// // const endpoint = "";
 
-fetch(`${endpoint}/api/hello`).then((res) => {
-    res.json().then((r) => {
+// fetch(`${endpoint}/api/hello`).then((res) => {
+//     res.json().then((r) => {
+//         console.log(r);
+//     });
+// });
+
+async function test() {
+    const url = "https://racematevercel.vercel.app/api/users"; // <-- replace this
+    // const url = "https://racematevercel.vercel.app/api/hello"; // <-- replace this
+  
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: "testuser@example.com",
+            password: "mypassword123"
+        })
+    });
+  
+    const result = await response;
+    console.log(result);
+    result.json().then((r) => {
         console.log(r);
     });
-});
+}
+
+test().catch(console.error);
--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-fetch("https://ep-nameless-bonus-afnj8oah.apirest.c-2.us-west-2.aws.neon.tech/neondb/rest/v1").then((res) => {
-    console.log(res);
-});
+async function test() {
+    // const url = "https://racematevercel.vercel.app/api/users"; // <-- replace this
+    const url = "https://racematevercel.vercel.app/api/hello"; // <-- replace this
+  
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: "testuser@example.com",
+            password: "mypassword123"
+        })
+    });
+  
+    const result = await response;
+    console.log(result);
+    // console.log(result);
+}
+
+test().catch(console.error);
