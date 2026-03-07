3010080d78c419db14ecc539ed3f71b2b193e29a
Parker Harris <rparkerharris@gmail.com>
2026-03-07 15:28:04 -0800

    Sample users api call

--- api/auth/signUp.js
+++ api/auth/signUp.js
@@ -15,10 +15,19 @@ module.exports.default = async function handler(req, res) {
 
     req.on("end", () => {
         try {
-            const parsed = JSON.parse(body);
-            const { myVar } = parsed;
+            // const parsed = JSON.parse(body);
+            // const { myVar } = parsed;
+            // res.writeHead(200, { "Content-Type": "application/json" });
+            // res.end(JSON.stringify({ message: myVar }));
+
+            const users = await sql`
+                SELECT id, name, email, created_at
+                FROM users
+                ORDER BY id ASC
+            `;
             res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ message: myVar }));
+            res.end(JSON.stringify({ message: users }));
+
         } catch (err) {
             res.writeHead(500, { "Content-Type": "application/json" });
             res.end(JSON.stringify({ errror: err ? err.message : "Internal server error" }));
--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 
-fetch("http://localhost:3000/api/auth/signUp",  {
+
+fetch("https://racematevercel.vercel.app/api/auth/signUp",  {
+// fetch("http://localhost:3000/api/auth/signUp",  {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
   },
   body: JSON.stringify({
-    myVar: "my poopy butthole"
+    myVar: "my peepee"
   })
 }).then((res) => {
   res.json().then((r) => {
