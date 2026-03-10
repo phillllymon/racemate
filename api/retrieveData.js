14ed63b4d5016fd79a48a448f3e7e163c0a1ca08
Parker Harris <rparkerharris@gmail.com>
2026-03-09 18:27:55 -0700

    Use dynamic target for search table

--- api/retrieveData.js
+++ api/retrieveData.js
@@ -61,7 +61,7 @@ module.exports.default = async function handler(req, res) {
                                 // WHERE ${conditionStrs.join(" AND ")}
 
                                 sql`
-                                    SELECT * FROM boats
+                                    SELECT * FROM ${target}
                                 `.then((items) => {
                                     res.writeHead(200, { "Content-Type": "application/json" });
                                     res.end(JSON.stringify({
